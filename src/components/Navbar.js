2e313d029c70cba563b54fd97adfb05f48f79e1e
Espen Nilsen <post@espennilsen.net>
2019-02-05 18:43:57 +0000

    Fixing the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -49,20 +49,11 @@ const Navbar = class extends React.Component {
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-        <Link className="navbar-item" to="/contact">
-          Contact
-        </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
       </div>
       <div className="navbar-end has-text-centered">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/espennilsen"
           target="_blank"
           rel="noopener noreferrer"
         >
